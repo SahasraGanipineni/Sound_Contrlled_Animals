dbdff34d4415512395f21717e861d99dc59b4e64
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-11-06 09:37:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,34 @@ function  modelReady()
 }
 function gotResults(error, results)
 {
-    console.log('Got Result');
+    if(error) {
+        console.error(error);
+    } else{
+        console.log(results);
+    
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+
+        img = document.getElementById('image');
+
+        if(results[0].label == "Dog Barking")
+        {
+            img.src = 'dog.png';
+        }
+        else if(results[0].label == "Cat Meowing")
+        {
+            img.src = 'cat.png';
+        }
+        else if(results[0].label == "Lion Roaring")
+        {
+            img.src = 'lion.png';
+        }
+        else if(results[0].label == "Elephant Sound")
+        {
+            img.src = 'elephant.png';
+        }
+        else
+        {
+            img.src = 'hearing.png';
+        }
+    }
 }
